60b7654800798f6949caf96c7d749b803cd560af
Cauã <cauacampos05@gmail.com>
2021-09-01 23:55:48 -0300

    CONCLUSAO FINAL 2

--- src/components/Img.e.Botão/botao.js
+++ src/components/Img.e.Botão/botao.js
@@ -1,5 +1,14 @@
+import Texto from "../Titulos.Paragrafos/Titulo";
 import "./Img.e.botao.css";
 
-export default function Botao() {
-    return <button><a href="" target="blank">Visualizar</a></button>
+export default function Botao(props) {
+    return <button><a href={props.texto} target="blank">Visualizar</a></button>
+}
+
+export function botao() {
+    return(
+        <div>
+            <Botao href=""/>
+        </div>
+    )
 }
